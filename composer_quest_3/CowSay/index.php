deec6145e13be71b1f6bf8615066209631a3ba9c
Martin Lerude <77109266+Nitram-Edurel@users.noreply.github.com>
2021-10-18 19:40:08 +0200

    Update index.php

--- composer_quest_3/CowSay/index.php
+++ composer_quest_3/CowSay/index.php
@@ -5,9 +5,10 @@ require 'vendor/autoload.php';
 use CowSay\Cow;
 
 $bessie = new Cow('Hello, Farm!');
-
-// store the output in a variable
-$output = $bessie->say();
-echo $output;
+$bessie->setEyes('oO')
+    ->setTongue('U')
+    ->setPoop('@@@')
+    ->setUdder('W');
+echo $bessie;
 
 
